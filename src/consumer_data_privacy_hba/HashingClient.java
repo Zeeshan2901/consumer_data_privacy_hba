938dbc11004b4a7022677b3cba230cb5bea420da
zeeshan2901 <syedzeeshanahmed@live.in>
2019-07-06 23:17:10 -0300

    new

--- src/consumer_data_privacy_hba/HashingClient.java
+++ src/consumer_data_privacy_hba/HashingClient.java
@@ -1,16 +1,18 @@
 package consumer_data_privacy_hba;
 
-public class HashiingClient {
+public class HashingClient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		ConsumerDataPrivacyHBA obj = new ConsumerDataPrivacyHBA();
-		obj.readFromAlice();
-		obj.readFromBob();
-		obj.hashMatch();
+		//obj.readFromAlice();
+		//obj.readFromBob();
+		//obj.hashMatch();
 		obj.readFile("input/alice.txt");
 		//obj.readFile("input/bob.txt");
+		System.out.println("\nContents of LocGene : \n" +obj.locGene);
+		System.out.println("\nContents of LocRsid : \n" +obj.locRsid);
 		
 
 	}
